2d403f4631fc79fff8e1fc5fcdc384523f8777f0
luisbesil289 <l.besil@gmail.com>
2019-06-14 08:08:17 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React from 'react'
-import { Container, Button } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+import React from 'react';
+import './App.css';
 
-import Mensaje from './Mensaje'
-import MiPrimerComponente from './Root'
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Container textAlign='center'>
-          <Mensaje size='16px' color='#ff0000' contenido='AHORA' />
-          <Mensaje size='14px' color='#00ff00' contenido='ENTIENDO' />
-          <Mensaje size='10px' color='#0000ff' contenido='LOS PROPS :D' />
-          <Button content='Boton Coooool' color='blue' />
-          <MiPrimerComponente />
-        </Container>
+        <h1>Hello World</h1>
       </div>
     )
   }
