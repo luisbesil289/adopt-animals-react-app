93e3634179a12d547cc124baa1c78b0746cf1b7a
luisbesil289 <l.besil@gmail.com>
2019-06-18 19:44:18 -0300

    .

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Wishlist from './Wishlist';
 
 
 
@@ -24,11 +25,34 @@ class Padre extends React.Component{
   }
 }
 
-class Hija extends React.Component{
+class Hija extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      section: 0
+    };
+  }
+
+  goToWishlist = () => {
+    this.setState({
+      section: 4
+    });
+  }
+
+  currentSection() {    
+    if (this.state.section === 4) {      
+      return <Wishlist />;
+    }
+  }
+
   render() {
     return (
       <div>
-        <h3>test</h3>
+        <h3>Card</h3>
+        <a className="nav-link" href="#Wishlist" onClick={this.goToWishlist}>Wishlist <span className="sr-only"></span></a>
+              
+        <button className="btn btn-primary" onClick={this.goToWishlist}>Ver</button>
+        {this.currentSection()}
     </div>
     );
   }
